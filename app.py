4594c7445d6400f38c2f9ad5e3adca14f701dd40
howard <howard518889@gmail.com>
2026-05-14 20:13:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,6 @@ html_files = {
     "光度密度3D體積圖": "50PC_luminosity.html",
     "次數分布圖": "50PC_hist_filtered.html",
     "星色星等圖": "50PC_HR_Diagram.html",
-    "恆星演化階段恆星分布圖": "50PC_HR_Scatter.html",
     "恆星密度次數分布圖": "50PC_hist_filtered.html",
     "恆星密度3D體積圖與銀河旋臂疊圖": "疏散星團分布.html",
 }
